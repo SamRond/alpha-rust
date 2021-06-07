9be10783b999ae7f0a5d0c0eaa7c3f77914713b6
Sam Rondinelli <sam.rondinelli3@gmail.com>
2021-06-07 01:43:20 -0600

    added new test case

--- tests/board.rs
+++ tests/board.rs
@@ -1,16 +1,17 @@
 use alpha_rust::Board;
 
-fn init() -> Board {
-    Board::new("".to_string())
+mod utils;
+
+fn init(fen:String) -> Board {
+    utils::set_panic_hook();
+    Board::new(fen)
 }
 
 #[cfg(test)]
 mod tests {
-    use super::Board;
-
     #[test]
     fn test_board_init() {
-        let board = crate::init();
+        let board = crate::init("".to_string());
 
         print!("\n\n");
 
@@ -46,14 +47,35 @@ mod tests {
     }
 
     #[test]
+    fn test_board_init_with_fen() {
+        let mut board = crate::init("rnbqkbnr/pp1ppppp/8/2p5/4P3/5N2/PPPP1PPP/RNBQKB1R b KQkq - 1 2".to_string());
+
+        print!("Checking if FEN is set to correct value... ");
+        assert_eq!(board.get_fen(), "rnbqkbnr/pp1ppppp/8/2p5/4P3/5N2/PPPP1PPP/RNBQKB1R b KQkq - 1 2");
+        println!("true");
+
+        print!("Checking if white e pawn is in correct position... ");
+        assert_eq!(board.get_white_pieces()[0].get_position(), (4, 5));
+        println!("true");
+
+        print!("Checking if black c pawn is in correct position... ");
+        assert_eq!(board.get_black_pieces()[7].get_position(), (5, 3));
+        println!("true");
+
+        print!("Checking if white knight is in correct position... ");
+        assert_eq!(board.get_white_pieces()[10].get_position(), (3, 6));
+        println!("true");
+    }
+
+    #[test]
     fn test_pawn_move() {
-        let mut board = crate::init();
+        let mut board = crate::init("".to_string());
 
         print!("\n\n");
 
         // make move 1. e4
         print!("Checking if pawn move is successful... ");
-        let pawn = &board.get_white_pieces()[0];
+        let pawn = &board.get_white_pieces()[4];
 
         let mv = board.make_move(pawn, 4, 5);
         assert!(mv);
